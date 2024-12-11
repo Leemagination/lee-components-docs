e2e7d3340c4fa21624029b612dd5c47f7c867a2e
Leemagination <328445543@qq.com>
2024-12-11 23:08:00 +0800

    feat: 修改侧边栏配置

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -8,51 +8,76 @@ export default defineUserConfig({
   title: '一个用于学习的vue3组件库',
   description: '',
   theme: defaultTheme({
+    editLink: false,
+    repo: 'https://github.com/Leemagination/vue3-components',
     colorModeSwitch: false,
     themePlugins: {
       backToTop: false
     },
     sidebar: [
       '/',
-      '/button/',
-      '/alert/',
-      '/backTop/',
-      '/breadcrumb/',
-      '/calender/',
-      '/card/',
-      '/cascader/',
-      '/checkbox/',
-      '/collapse/',
-      '/datePicker/',
-      '/divider/',
-      '/drawer/',
-      '/dropdown/',
-      '/input/',
-      '/inputNumber/',
-      '/list/',
-      '/loading/',
-      '/message/',
-      '/modal/',
-      '/notification/',
-      '/pagination/',
-      '/popconfirm/',
-      '/popover/',
-      '/progress/',
-      '/radio/',
-      '/rate/',
-      '/select/',
-      '/slider/',
-      '/steps/',
-      '/switch/',
-      '/tabs/',
-      '/tag/',
-      '/timeline/',
-      '/timePicker/',
-      '/tooltip/',
-      '/tree/'
+      {
+        text: '简单组件',
+        collapsible: true,
+        children: [
+          '/button/',
+          '/alert/',
+          '/backTop/',
+          '/breadcrumb/',
+          '/card/',
+          '/checkbox/',
+          '/collapse/',
+          '/divider/',
+          '/list/',
+          '/loading/',
+          '/progress/',
+          '/radio/',
+          '/rate/',
+          '/slider/',
+          '/steps/',
+          '/switch/',
+          '/tag/',
+          '/timeline/',
+        ]
+      },
+      {
+        text: '浮层组件',
+        collapsible: true,
+        children: [
+          '/cascader/',
+          '/datePicker/',
+          '/dropdown/',
+          '/popconfirm/',
+          '/popover/',
+          '/select/',
+          '/timePicker/',
+          '/tooltip/',
+        ]
+      },
+      {
+        text: '命令式组件',
+        collapsible: true,
+        children: [
+          '/drawer/',
+          '/message/',
+          '/modal/',
+          '/notification/',
+        ]
+      },
+      {
+        text: '复杂组件',
+        collapsible: true,
+        children: [
+          '/input/',
+          '/inputNumber/',
+          '/pagination/',
+          '/calender/',
+          '/tabs/',
+          '/tree/'
+        ]
+      }
     ],
     sidebarDepth: 0,
-    //navbar: ['/', '/button/'],
   }),
   plugins: [
     prismjsPlugin({
